6d2ecf3a626a13cf88b9ee09a7d8e74fef53c678
joe-main <qmmplusfact@gmail.com>
2022-01-27 21:19:03 +0900

    コメント追記

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 //React処理を書いているIndex.tsxを読み込んでいる
 import { Index } from './Index';
 
-//index.htmlのid='root'の<div>要素内にAppを描画
+//index.htmlのid='root'の<div>要素内にIndexを描画
 ReactDOM.render(<Index />, document.getElementById('root'));
